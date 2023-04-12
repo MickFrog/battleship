fd61562013a629c0ea0e6bc6bcb75240aa7fd8c1
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-04-12 10:14:54 +0300

    write randomize single ship function + utilities

--- src/player.js
+++ src/player.js
@@ -11,7 +11,71 @@ export default class Player {
         this.#generateMoves(); //generate possible moves
     }
 
-    
+    #randomizeShip(shipLength) { // This is for placing ships on the AI board.
+        const axes = ['x', 'y']; 
+        const currAxis = axes[Math.floor(Math.random() * axes.length)];
+
+        let newPlace = [];
+        if (currAxis == 'x') {
+            let isValid = false;
+            while (!isValid) {
+                newPlace = this.#randomHorizontal(shipLength);
+
+                if (this.#checkValidity(newPlace)) {
+                    isValid = true;
+                }  
+            }
+        }
+
+        if (currAxis == 'y') {
+            let isValid = false;
+            while (!isValid) {
+                newPlace = this.#randomVertical(shipLength);
+
+                if (this.#checkValidity(newPlace)) {
+                    isValid = true;
+                }
+            }
+        }
+
+        return newPlace;
+    }
+
+    #randomHorizontal(shipLength) {
+        let positions = [];
+        
+        //generate random letter from A(65) to J(74) 
+        const currLetterCode = Math.floor(Math.random() * (74 - 65 + 1)) + 65; 
+        const currLetter = String.fromCharCode(currLetterCode);
+
+        //generate random number between 0 and (10-shipLength)
+        let currNumber = Math.floor(Math.random() * (10 - shipLength));
+
+        //generate positions for the horizontal line.
+        for (let i = 0; i < shipLength; i++) {
+            positions.push(`${currLetter}${currNumber}`);
+            currNumber++;
+        }
+
+        return positions;
+    }
+
+    #randomVertical(shipLength) {
+        let positions = [];
+
+        //generate random number between 0 and 10
+        const currNumber = Math.floor(Math.random() * 10);
+
+        //generate random letter from A(65) to J-shipLength(74-shipLength)
+        let currLetterCode = Math.floor(Math.random() * ((74 - shipLength) - 65 + 1)) + 65;
+        
+        //generate positions for vertical ship placement
+        for (let i = 0; i < shipLength; i++) {
+            positions.push(`${String.fromCharCode(currLetterCode)}${currNumber}`);
+            currLetterCode++;
+        }
+        return positions;
+    }
 
     #generateMoves() {
         for(let i = 65; i < 75; i++){ // A to J
@@ -21,6 +85,15 @@ export default class Player {
                 this.possibleMoves.push(`${letter}${j}`);
             }
         }
+    }
+
+    #checkValidity(posArray) { //check if all positions provided are valid
+        for(let pos of posArray) {
+            if (this.playerBoard.gameBoard.get(pos)[0] != null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     #shuffleArray(myArr) {
