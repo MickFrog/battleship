6730009b314f9c5fc01c2050098cdaeff55f30f3
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-04-15 14:24:38 +0300

    add hover of placing board acc. to ship length

--- src/domBusiness/dom.js
+++ src/domBusiness/dom.js
@@ -4,6 +4,11 @@ const DOM_Elements = {
     placingBoard: document.getElementById('placingBoard'),
 }
 
+const placingUtil = {
+    shipLengths: [5, 4, 3, 3, 2],
+    currShipLen: 0,
+}
+
 function drawPositions(boardElem, bhover=false) {
     for (let i = 65; i < 75; i++) {
 
@@ -12,6 +17,14 @@ function drawPositions(boardElem, bhover=false) {
             newBox.id = `${String.fromCharCode(i)}${j}`;
             boardElem.appendChild(newBox);
         }
+    }
+}
+
+function addPlacingEvents() { //add hover events to all placingBoard boxes
+    let placingBoxes = [...DOM_Elements.placingBoard.childNodes]; 
+
+    for (let i = 0; i < placingBoxes.length; i++) {
+        placingBoardEvents(placingBoxes[i]);
     }
 }
 
@@ -26,12 +39,60 @@ function createBox(bhover=false) {
     return newBox;
 }
 
-function addBoxEvents(boxElem) {
-    
+function placingBoardEvents(boxElem) {
+    //Hover effects 
+    boxElem.addEventListener('mouseenter', () => {
+        let locations = generateLocations(boxElem.id); //generate locations from currBox to last box of ship
+        
+        if (isValidPlacing(locations)) {
+            setHovered(locations, true);
+        }
+    });
+
+    boxElem.addEventListener('mouseleave', () => {
+        const locations = generateLocations(boxElem.id);
+        setHovered(locations);
+    });
+}
+
+function generateLocations(elemID) {
+    let locations = [];
+    let shipLength = placingUtil.shipLengths[placingUtil.currShipLen];
+        
+    if (9 - parseInt(elemID[1]) + 1 >= shipLength) { //if ship can fit horizontally
+        //generate locations
+        for (let i = 0; i < shipLength; i++) {
+            locations.push(`${elemID[0]}${parseInt(elemID[1]) + i}`);
+        }
+    }
+    return locations;
+}
+
+function setHovered(posArray, bSwitch=false) {
+    if (posArray.length < 1) return;
+
+    const placingBoardChildren = [...DOM_Elements.placingBoard.children];
+    if (bSwitch) {
+        for (let i = 0; i < posArray.length; i++) {
+            const currBox = placingBoardChildren.find(element => element.id == posArray[i]);
+            currBox.classList.add('shipHover');
+        }
+        return;
+    }
+
+    for (let i = 0; i < posArray.length; i++) {
+        const currBox = placingBoardChildren.find(element => element.id == posArray[i]);
+        currBox.classList.remove('shipHover');
+    }
+}
+
+function isValidPlacing(posArray) {
+    return true;
 }
 
 export default function initializeBoards() {
     drawPositions(DOM_Elements.humanBoard);
     drawPositions(DOM_Elements.compBoard, true);
-    drawPositions(DOM_Elements.placingBoard, true);
+    drawPositions(DOM_Elements.placingBoard);
+    addPlacingEvents();
 }
