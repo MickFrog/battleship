de8bbe57dcf9e0951a867b5109c8eca408e4d607
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-04-21 00:05:45 +0300

    add place ships ability on placingBoard

--- src/domBusiness/placeShip.js
+++ src/domBusiness/placeShip.js
@@ -2,8 +2,13 @@ import { DOM_Elements, drawPositions } from "./domUtil";
 import { players } from "../gameHandler";
 
 const placingUtil = {
-    shipLengths: [5, 4, 3, 3, 2],
-    currShipLen: 0,
+    shipsToPlace: 
+        [{name: 'Carrier', len: 5}, 
+        {name: 'BattleShip', len: 4}, 
+        {name: 'Cruiser', len: 3}, 
+        {name: 'Submarine', len: 3},
+        {name: 'Destroyer', len: 2}],
+    currShip: 0,
     currAxis: 'x',
 }
 
@@ -28,6 +33,8 @@ function placingBoardEvents(boxElem) {
     let locations = [];
     //Hover effects 
     boxElem.addEventListener('mouseenter', () => {
+        if (placingUtil.currShip > 4) return; //prevent choosing non-existent ships
+
         locations = generateLocations(boxElem.id, placingUtil.currAxis); //generate locations from currBox to last box of ship
         
         if (isValidPlacing(locations)) {
@@ -36,13 +43,32 @@ function placingBoardEvents(boxElem) {
     });
 
     boxElem.addEventListener('mouseleave', () => {
+        if (placingUtil.currShip > 4) return; //prevent choosing non-existent ships
+
         setHovered(locations);
     });
+
+    boxElem.addEventListener('click', () => {
+        if (placingUtil.currShip > 4) return; //prevent choosing non-existent ships
+
+        if (isValidPlacing(locations)) {
+            //logically place ships
+            players.activePlayer.playerBoard.placeShip(placingUtil.shipsToPlace[placingUtil.currShip].name, 
+            placingUtil.shipsToPlace[placingUtil.currShip].len, 
+            locations);
+
+            //increment currShip
+            placingUtil.currShip++;
+
+            //visually place ships
+            setClicked(locations);
+        }
+    })
 }
 
 function generateLocations(elemID, axis) {
     let locations = [];
-    let shipLength = placingUtil.shipLengths[placingUtil.currShipLen];
+    let shipLength = placingUtil.shipsToPlace[placingUtil.currShip].len;
 
     if (axis == 'x') {
         if (9 - parseInt(elemID[1]) + 1 >= shipLength) { //if ship can fit horizontally
@@ -65,7 +91,7 @@ function generateLocations(elemID, axis) {
     return locations;
 }
 
-function setHovered(posArray, bSwitch=false) {
+function setHovered(posArray, bSwitch=false) { //bSwitch controls on and off of hover
     if (posArray.length < 1) return;
 
     const placingBoardChildren = [...DOM_Elements.placingBoard.children];
@@ -80,6 +106,16 @@ function setHovered(posArray, bSwitch=false) {
     for (let i = 0; i < posArray.length; i++) {
         const currBox = placingBoardChildren.find(element => element.id == posArray[i]);
         currBox.classList.remove('shipHover');
+    }
+}
+
+function setClicked(posArray) {
+    if (posArray.length < 1) return;
+
+    const placingBoardChildren = [...DOM_Elements.placingBoard.children];
+    for (let i = 0; i < posArray.length; i++) {
+        const currBox = placingBoardChildren.find(element => element.id == posArray[i]);
+        currBox.classList.add('shipBox');
     }
 }
 
